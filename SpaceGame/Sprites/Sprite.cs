080475a2e9104148a90934c985d5c07aa42e457b
Campbell Brown <campbellbrown093@gmail.com>
2020-01-27 19:12:29 +1300

    Added basic sprite class

---
+++ SpaceGame/Sprites/Sprite.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Sprites
+{
+    public class Sprite
+    {
+        protected Vector2 position;
+        protected Texture2D texture;
+        protected readonly float scale = 1f;
+        protected float rotation;
+        protected Vector2 Center { get { return new Vector2(texture.Width / 2f, texture.Height / 2f); } }
+
+        public Sprite(Vector2 position, Texture2D texture)
+        {
+            this.position = position;
+            this.texture = texture;
+            rotation = 0f;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, rotation, Center, scale, SpriteEffects.None, 0f);
+        }
+    }
+}
