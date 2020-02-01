f8426f607ea170cb6f3dc1575a9122a2fe8c5ccd
Campbell Brown <campbellbrown093@gmail.com>
2020-02-01 17:20:26 +1300

    Added rotation and velocity to particles

--- SpaceGame/Effects/Particle.cs
+++ SpaceGame/Effects/Particle.cs
@@ -14,17 +14,25 @@ namespace SpaceGame.Effects
         protected Texture2D texture;
         protected float lifeTime;
         protected float currentLifeTime;
+        protected float rotation;
+        protected float angularVelocity;
+        protected Vector2 linearVelocity;
 
         public Particle(Vector2 position, Texture2D texture, float lifeTime = 0f)
         {
             this.position = position;
             this.texture = texture;
             this.lifeTime = lifeTime;
+            angularVelocity = Game1.r.Next(-10, 11);
+            linearVelocity = new Vector2(Game1.r.Next(-10, 11), Game1.r.Next(-10, 11));
         }
 
         public void Update(GameTime gameTime)
         {
-            currentLifeTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float t = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position += linearVelocity * t;
+            rotation += angularVelocity * t;
+            currentLifeTime += t;
         }
 
         public void Draw(SpriteBatch spriteBatch)
