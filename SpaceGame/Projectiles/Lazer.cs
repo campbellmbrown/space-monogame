b169a4712125d6442897f7478bcf4e1d79fae39b
Campbell Brown <campbellbrown093@gmail.com>
2020-02-07 14:32:29 +1300

    FEATURE: Added projectile and lazer class

---
+++ SpaceGame/Projectiles/Lazer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Projectiles
+{
+    public class Lazer : Projectile 
+    {
+        public Lazer(Vector2 position, float rotation, Color color, Vector2 linearVelocity)
+            : base(position, Game1.textures["lazer"], rotation, linearVelocity)
+        {
+            base.color = color;
+        }
+    }
+}
---
+++ SpaceGame/Projectiles/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Projectiles
+{
+    public class Projectile
+    {
+        protected Texture2D texture;
+        protected Vector2 position;
+        protected Vector2 linearVelocity;
+        protected float rotation;
+        protected Vector2 center { get { return new Vector2(texture.Width / 2f, texture.Height / 2f); } }
+        protected Color color = Color.White;
+
+        public Projectile(Vector2 position, Texture2D texture, float rotation, Vector2 linearVelocity)
+        {
+            this.position = position;
+            this.texture = texture;
+            this.rotation = rotation;
+            this.linearVelocity = linearVelocity;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float t = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position += linearVelocity * t;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, color, rotation, center, 1f, SpriteEffects.None, 1f);
+        }
+    }
+}
