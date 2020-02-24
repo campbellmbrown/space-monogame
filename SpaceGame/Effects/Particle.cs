5d85ebde44ac4b10e36ba9a0e66a06f4ce3dafb0
Campbell Brown <campbellbrown093@gmail.com>
2020-02-24 14:53:57 +1300

    Commented the particle class

--- SpaceGame/Effects/Particle.cs
+++ SpaceGame/Effects/Particle.cs
@@ -10,6 +10,9 @@ using System.Threading.Tasks;
 
 namespace SpaceGame.Effects
 {
+    /// <summary>
+    /// The way the particle acts and gets destroyed.
+    /// </summary>
     public enum ParticleDestroyType
     {
         LifeTime,
@@ -18,6 +21,9 @@ namespace SpaceGame.Effects
         Other
     }
 
+    /// <summary>
+    /// Base class for particles.
+    /// </summary>
     public class Particle
     {
         private Vector2 _position;
@@ -48,28 +54,50 @@ namespace SpaceGame.Effects
         public float fadeTime = 10f;
         public float shrinkTime = 10f;
 
+        /// <summary>
+        /// Creates a new instance of the Particle class, with specified parameters.
+        /// </summary>
+        /// <param name="position">X and Y starting coordinates.</param>
+        /// <param name="texture">Texture of the particle.</param>
+        /// <param name="randomize">Determines if the velocities are random.</param>
+        /// <param name="particleDestroyType">Way the particles are destroyed.</param>
         public Particle(Vector2 position, Texture2D texture, bool randomize, ParticleDestroyType particleDestroyType)
         {
             this.position = position;
             this.texture = texture;
             this.particleDestroyType = particleDestroyType;
-            if (randomize) RandomizeVelocities();
+            if (randomize) RandomizeVelocities(10, 10);
         }
 
+        /// <summary>
+        /// Creates a new instance of the Particle class, with specified parameters.
+        /// </summary>
+        /// <param name="position">X and Y starting coordinates.</param>
+        /// <param name="animation">Animation of the particle.</param>
+        /// <param name="randomize">Determines if the velocities are random.</param>
         public Particle(Vector2 position, Animation animation, bool randomize)
         {
             animationManager = new AnimationManager(animation);
             this.position = position;
             this.lifeTime = animation.frameCount * animation.frameSpeed;
-            if (randomize) RandomizeVelocities();
+            if (randomize) RandomizeVelocities(10, 10);
         }
 
-        protected void RandomizeVelocities()
+        /// <summary>
+        /// Randomizes angular and random velocities.
+        /// </summary>
+        /// <param name="maxAngular">Maximum angular velocity.</param>
+        /// <param name="maxLinear">Maximum linear velocity.</param>
+        protected void RandomizeVelocities(int maxAngular, int maxLinear)
         {
-            angularVelocity = LimitsEdgeGame.r.Next(-10, 11);
-            linearVelocity = new Vector2(LimitsEdgeGame.r.Next(-10, 11), LimitsEdgeGame.r.Next(-10, 11));
+            angularVelocity = LimitsEdgeGame.r.Next(-maxAngular, maxAngular + 1);
+            linearVelocity = new Vector2(LimitsEdgeGame.r.Next(-maxLinear, maxLinear + 1), LimitsEdgeGame.r.Next(-maxLinear, maxLinear + 1));
         }
 
+        /// <summary>
+        /// Updates the particle.
+        /// </summary>
+        /// <param name="gameTime">GameTime instance.</param>
         public void Update(GameTime gameTime)
         {
             float t = (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -81,12 +109,19 @@ namespace SpaceGame.Effects
             if (_hasAnimation) animationManager.Update(gameTime);
         }
 
+        /// <summary>
+        /// Draws the particle.
+        /// </summary>
+        /// <param name="spriteBatch">SpriteBatch instance.</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             if (_hasAnimation) animationManager.Draw(spriteBatch, rotation);
             else if (_hasTexture) spriteBatch.Draw(texture, position, textureRectangle, Color.White * opacity, rotation, center, scale, SpriteEffects.None, 0f);
         }
 
+        /// <summary>
+        /// Returns true if the particle should be destroyed.
+        /// </summary>
         public bool CheckToDestroy()
         {
             bool toDestroy;
@@ -108,6 +143,9 @@ namespace SpaceGame.Effects
             return toDestroy;
         }
 
+        /// <summary>
+        /// Returns true if the particle has exceeded it's lifetime.
+        /// </summary>
         public bool ExceedsLifeTime()
         {
             if (currentLifeTime > lifeTime)
@@ -115,6 +153,9 @@ namespace SpaceGame.Effects
             return false;
         }
 
+        /// <summary>
+        /// Returns true if the particle has faded to nothing.
+        /// </summary>
         public bool FadedToNothing()
         {
             if (opacity <= 0)
@@ -122,6 +163,9 @@ namespace SpaceGame.Effects
             return false;
         }
 
+        /// <summary>
+        /// Returns true if the particle has shrank to nothing.
+        /// </summary>
         public bool ShrankToNothing()
         {
             if (scale <= 0)
