56c6ff86f23f52effd9cb09e5e6ccd5cacc64ee0
Campbell Brown <campbellbrown093@gmail.com>
2020-03-24 18:23:50 +1300

    Updated person pathfinding

--- SpaceGame/Sprites/ShipStateSprites/Person.cs
+++ SpaceGame/Sprites/ShipStateSprites/Person.cs
@@ -15,60 +15,90 @@ namespace SpaceGame.Sprites.ShipStateSprites
     {
         protected List<Vector2> pathToTake;
         protected List<PathNode> nodes;
+        protected Vector2 currentObjective;
+        protected List<ShipFloor> walkableTiles;
+        protected float currentIdleDelay = 0;
+        protected float idleDelay = 0;
 
-        public Person(Vector2 position, Texture2D texture) : base(position, texture)
+        public Person(Vector2 position, Texture2D texture, List<ShipFloor> walkableTiles) : base(position, texture)
         {
+            this.walkableTiles = walkableTiles;
             pathToTake = new List<Vector2>();
             nodes = new List<PathNode>();
+            CreateNodes(walkableTiles);
+            CreatePath();
         }
 
-        public Person(Vector2 position, Animation animation) : base(position, animation)
+        public Person(Vector2 position, Animation animation, List<ShipFloor> walkableTiles) : base(position, animation)
         {
+            this.walkableTiles = walkableTiles;
             pathToTake = new List<Vector2>();
             nodes = new List<PathNode>();
+            CreateNodes(walkableTiles);
+            CreatePath();
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            foreach (var pos in pathToTake) spriteBatch.DrawRectangle(new Rectangle((int)pos.X, (int)pos.Y, 8, 8), Color.Red);
             base.Draw(spriteBatch);
         }
 
-        bool createdPath = false;
         public override void Update(GameTime gameTime)
         {
-            if (!createdPath) { CreatePath(); createdPath = true; }
+            float t = (float)gameTime.ElapsedGameTime.TotalSeconds;
             base.Update(gameTime);
+            if (currentIdleDelay >= idleDelay)
+            {
+                Vector2 difference = currentObjective - position;
+                if (difference.Length() < 0.2f)
+                {
+                    pathToTake.Remove(currentObjective);
+                    if (pathToTake.Count == 0)
+                    {
+                        idleDelay = LimitsEdgeGame.r.Next(0, 31);
+                        currentIdleDelay = 0;
+                        CreateNodes(walkableTiles);
+                        CreatePath();
+                    }
+                    else currentObjective = pathToTake.ElementAt(0);
+                }
+                else
+                {
+                    Vector2 direction = Vector2.Normalize(difference);
+                    position += direction * 15f * t;
+              }
+            } else currentIdleDelay += t;
         }
 
         public void CreatePath()
         {
-            CreateNodes();
-
             List<PathNode> unvisitedNodes = nodes.ToList();
             List<PathNode> visitedNodes = new List<PathNode>();
             PathNode currentNode;
             PathNode endNode;
 
-            int startingNodeIndex = LimitsEdgeGame.r.Next(0, unvisitedNodes.Count);
-            int endNodeIndex = LimitsEdgeGame.r.Next(0, unvisitedNodes.Count);
+            int xToSearch = (int)Math.Round(position.X - Tile.tileSize / 2f, 0);
+            int yToSearch = (int)Math.Round(position.Y - Tile.tileSize / 2f, 0);
+            int startingNodeIndex = unvisitedNodes.FindIndex(node => node.X == xToSearch && node.Y == yToSearch);
+
+            if (startingNodeIndex == -1) // Failsafe - if a node at the person's location is not found, generate a random place to go.
+                startingNodeIndex = LimitsEdgeGame.r.Next(0, unvisitedNodes.Count);
+
             unvisitedNodes[startingNodeIndex].cost = 0;
             currentNode = unvisitedNodes.ElementAt(startingNodeIndex);
-            endNode = unvisitedNodes.ElementAt(endNodeIndex);
+            endNode = unvisitedNodes.ElementAt(LimitsEdgeGame.r.Next(0, unvisitedNodes.Count));
             currentNode.cost = 0;
-            // Add current node to the visited list and remove from the unvisted list
+
             var selected = unvisitedNodes.Where(node => node.nodeID == currentNode.nodeID).ToList();
             unvisitedNodes = unvisitedNodes.Except(selected).ToList();
             visitedNodes.AddRange(selected);
+            
             bool finishedRoute = false;
             while (!finishedRoute)
             {
-                // look at the current node neighbors.
                 foreach (var neighborNodeID in currentNode.connectedNodes)
                 {
-                    // Take the cost of the current node plus the distance to each of the neighbors.
                     int neighborCost = currentNode.cost + 1;
-                    // Update the costs at the neighbors if the cost is less than the neighbors current tentative distance
                     int neighborNodeIndex = unvisitedNodes.FindIndex(node => node.nodeID == neighborNodeID);
                     if (neighborNodeIndex >= 0)
                     {
@@ -76,51 +106,49 @@ namespace SpaceGame.Sprites.ShipStateSprites
                         {
                             unvisitedNodes[neighborNodeIndex].cost = neighborCost;
                             unvisitedNodes[neighborNodeIndex].stepsTo = new List<Vector2>(currentNode.stepsTo);
-                            unvisitedNodes[neighborNodeIndex].stepsTo.Add(new Vector2(unvisitedNodes[neighborNodeIndex].X, unvisitedNodes[neighborNodeIndex].Y));
+                            unvisitedNodes[neighborNodeIndex].stepsTo.Add(
+                                new Vector2(unvisitedNodes[neighborNodeIndex].X + Tile.tileSize / 2f, unvisitedNodes[neighborNodeIndex].Y + Tile.tileSize / 2f));
                         }
                     }
                 }
-                // Go to the unvisited node with the lowest tentative distance and set this as the current node
                 int lowestCost = unvisitedNodes[0].cost;
                 int nextNodeID = unvisitedNodes[0].nodeID;
                 foreach (var unvisitedNode in unvisitedNodes)
-                {
                     if (unvisitedNode.cost < lowestCost)
                     {
                         nextNodeID = unvisitedNode.nodeID;
                         lowestCost = unvisitedNode.cost;
                     }
-                }
-                // Add the new current node to the list of visited nodes and remove from the list of unvisited nodes
+
                 currentNode = unvisitedNodes[unvisitedNodes.FindIndex(node => node.nodeID == nextNodeID)];
                 var nextNode = unvisitedNodes.Where(node => node.nodeID == currentNode.nodeID).ToList();
                 unvisitedNodes = unvisitedNodes.Except(nextNode).ToList();
                 visitedNodes.AddRange(nextNode);
-                // If the final one is in the list of visited nodes, finish
-                if (visitedNodes.Exists(node => node.nodeID == endNode.nodeID))
-                {
-                    finishedRoute = true;
+                if (visitedNodes.Exists(node => node.nodeID == endNode.nodeID)) 
+                { 
+                    finishedRoute = true; 
                     pathToTake = endNode.stepsTo.ToList();
+                    // index out of range here for some reason
+                    if (pathToTake.Count != 0) currentObjective = pathToTake.ElementAt(0);
                 }
             }
         }
 
-        protected void CreateNodes()
+        protected void CreateNodes(List<ShipFloor> walkableTiles)
         {
-            foreach (var walkableTile in LimitsEdgeGame.shipStateManager.tileManager.walkableTiles)
+            nodes.Clear();
+            foreach (var walkableTile in walkableTiles)
             {
                 PathNode pathNode = new PathNode(walkableTile.X, walkableTile.Y, walkableTile.floorID);
-
                 List<Point> coordsToCheck = new List<Point>();
                 coordsToCheck.Add(new Point(walkableTile.X - Tile.tileSize, walkableTile.Y));
                 coordsToCheck.Add(new Point(walkableTile.X, walkableTile.Y + Tile.tileSize));
                 coordsToCheck.Add(new Point(walkableTile.X + Tile.tileSize, walkableTile.Y));
                 coordsToCheck.Add(new Point(walkableTile.X, walkableTile.Y - Tile.tileSize));
-
                 foreach (var p in coordsToCheck)
                 {
                     Predicate<ShipFloor> offsetPosition = (ShipFloor pos) => { return pos.X == p.X && pos.Y == p.Y; };
-                    ShipFloor foundNeighbor = LimitsEdgeGame.shipStateManager.tileManager.walkableTiles.Find((ShipFloor pos) => { return pos.X == p.X && pos.Y == p.Y; });
+                    ShipFloor foundNeighbor = walkableTiles.Find((ShipFloor pos) => { return pos.X == p.X && pos.Y == p.Y; });
                     if (foundNeighbor != null) pathNode.connectedNodes.Add(foundNeighbor.floorID);
                 }
                 nodes.Add(pathNode);
