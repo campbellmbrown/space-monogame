7eb1ad1372648a1ca59cb8ddd722a8c304cc8468
Campbell Brown <campbellbrown093@gmail.com>
2020-06-19 14:41:49 +1200

    Added label texture

--- SpaceGame/LimitsEdgeGame.cs
+++ SpaceGame/LimitsEdgeGame.cs
@@ -101,6 +101,7 @@ namespace SpaceGame
                 { "selection_bar_dark_highlighted", Content.Load<Texture2D>("GUI/selection_bar_dark_highlighted") },
                 { "item_slot", Content.Load<Texture2D>("GUI/item_slot") },
                 { "crew_menu", Content.Load<Texture2D>("GUI/crew_menu") },
+                { "label", Content.Load<Texture2D>("GUI/label") }
             };
 
             animations = new Dictionary<string, Animation>()
